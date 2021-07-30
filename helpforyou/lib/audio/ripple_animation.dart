8139490841709f9d65e6e92a25e4ff841605b6b2
eloise-takami <71730705+eloise-takami@users.noreply.github.com>
2021-07-30 17:09:26 -0300

    audio

--- helpforyou/lib/audio/ripple_animation.dart
+++ helpforyou/lib/audio/ripple_animation.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
 import 'package:helpforyou/shared/themes/app_colors.dart';
 import 'circle_painter.dart';
 import 'curve_wave.dart';
 import 'recorder_view.dart';
+import 'package:path_provider/path_provider.dart';
+import 'recorded_list_view.dart';
 
 class RipplesAnimation extends StatefulWidget {
   const RipplesAnimation({
@@ -23,6 +27,8 @@ class RipplesAnimation extends StatefulWidget {
 
 class _RipplesAnimationState extends State<RipplesAnimation>
     with TickerProviderStateMixin {
+  late Directory appDirectory;
+  List<String> records = [];
   late AnimationController _controller;
   @override
   void initState() {
@@ -31,40 +37,57 @@ class _RipplesAnimationState extends State<RipplesAnimation>
       duration: const Duration(milliseconds: 2000),
       vsync: this,
     )..repeat();
+    super.initState();
+    getApplicationDocumentsDirectory().then((value) {
+      appDirectory = value;
+      appDirectory.list().listen((onData) {
+        if (onData.path.contains('.aac')) records.add(onData.path);
+      }).onDone(() {
+        records = records.reversed.toList();
+        setState(() {});
+      });
+    });
   }
 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
+    appDirectory.delete();
   }
 
   Widget _button() {
     return Center(
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(widget.size),
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            gradient: RadialGradient(
-              colors: <Color>[
-                widget.color,
-                AppColors.roxo,
-                //Color.lerp(widget.color, Colors.black, .05),
-              ],
-            ),
-          ),
-          child: ScaleTransition(
-              scale: Tween(begin: 0.95, end: 1.0).animate(
-                CurvedAnimation(
-                  parent: _controller,
-                  curve: const CurveWave(),
-                ),
+      child: GestureDetector(
+        onTap: () {
+          //funcao parar de gravar
+          print("audio finalizado");
+        },
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(widget.size),
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              gradient: RadialGradient(
+                colors: <Color>[
+                  widget.color,
+                  AppColors.roxo,
+                  //Color.lerp(widget.color, Colors.black, .05),
+                ],
               ),
-              child: Icon(
-                Icons.keyboard_voice,
-                size: 44,
-                color: Colors.white,
-              )),
+            ),
+            child: ScaleTransition(
+                scale: Tween(begin: 0.95, end: 1.0).animate(
+                  CurvedAnimation(
+                    parent: _controller,
+                    curve: const CurveWave(),
+                  ),
+                ),
+                child: Icon(
+                  Icons.keyboard_voice,
+                  size: 44,
+                  color: Colors.white,
+                )),
+          ),
         ),
       ),
     );
