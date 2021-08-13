d82735b86d583ee21437cf84155c8f1328baf910
eloise-takami <71730705+eloise-takami@users.noreply.github.com>
2021-08-12 22:58:14 -0300

    new static final

--- helpforyou/lib/shared/themes/app_colors.dart
+++ helpforyou/lib/shared/themes/app_colors.dart
@@ -6,4 +6,6 @@ class AppColors {
   static const Color roxo = Colors.indigo; // .fromRGBO(62, 71, 208, 1.0);
 
   static final Color azulClaro = Colors.cyan.shade50;
+
+  static final Color branco = Colors.white;
 }
--- helpforyou/lib/shared/themes/app_images.dart
+++ helpforyou/lib/shared/themes/app_images.dart
@@ -1,4 +1,5 @@
 class Imagem {
   static const logo_nome = 'assets/images/logo_sembranco.png';
   static const logo_h = 'assets/images/h_sembranco.png';
+  static const medico = 'assets/images/medico.png';
 }
