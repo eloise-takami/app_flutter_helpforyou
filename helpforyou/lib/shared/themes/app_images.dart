da7524ae8733dc74d44f6651ed1ebf66a3be8265
eloise-takami <71730705+eloise-takami@users.noreply.github.com>
2021-08-23 23:29:40 -0300

    borda

--- helpforyou/lib/shared/themes/app_images.dart
+++ helpforyou/lib/shared/themes/app_images.dart
@@ -2,4 +2,5 @@ class Imagem {
   static const logo_nome = 'assets/images/logo_sembranco.png';
   static const logo_h = 'assets/images/h_sembranco.png';
   static const medico = 'assets/images/medico.png';
+  static const borda = 'assets/images/borda2.png';
 }
