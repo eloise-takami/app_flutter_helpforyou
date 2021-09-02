8b8a7bcf5fd5eb5b656234729e65e2ff02c3ba73
eloise-takami <71730705+eloise-takami@users.noreply.github.com>
2021-09-02 18:03:15 -0300

    SingleChildScrollView

--- helpforyou/lib/modules/feed/post/new_post_page.dart
+++ helpforyou/lib/modules/feed/post/new_post_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:helpforyou/shared/themes/app_colors.dart';
+import 'package:helpforyou/shared/themes/app_images.dart';
 
 class NewPostPage extends StatefulWidget {
   const NewPostPage({Key? key}) : super(key: key);
@@ -25,119 +27,146 @@ class _NewPostPageState extends State<NewPostPage> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(''),
-        backgroundColor: Color.fromRGBO(62, 71, 208, 1.0),
+        elevation: 0,
+        backgroundColor: AppColors.roxo,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            SizedBox(height: 20),
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  padding: EdgeInsets.only(left: 1, right: 2),
-                  decoration: new BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: DropdownButton<String>(
-                    hint: Text(
-                      " onde se aproxima esse relato",
-                      style: GoogleFonts.breeSerif(
-                        textStyle: Theme.of(context).textTheme.headline4,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w900,
-                        fontStyle: FontStyle.normal,
-                        color: Color.fromRGBO(62, 71, 208, 1.0),
-                      ),
-                    ),
-                    dropdownColor: Colors.white,
-                    icon: Icon(Icons.arrow_drop_down,
-                        color: Color.fromRGBO(62, 71, 208, 1.0)),
-                    iconSize: 30,
-                    style: GoogleFonts.breeSerif(
-                      textStyle: Theme.of(context).textTheme.headline4,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w900,
-                      fontStyle: FontStyle.normal,
-                      color: Color.fromRGBO(62, 71, 208, 1.0),
-                    ),
-                    value: valueChoose,
-                    onChanged: (newValue) {
-                      setState(() {
-                        valueChoose = newValue;
-                      });
-                    },
-                    items: listItem.map((valueItem) {
-                      return DropdownMenuItem(
-                        value: valueItem,
-                        child: Text(valueItem),
-                      );
-                    }).toList(),
+      body: Container(
+        color: AppColors.roxo,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 5),
+                height: 720,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: AppColors.branco,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(70),
+                    //topRight: Radius.circular(24)
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              maxLength: 200,
-              maxLines: 7,
-              decoration: InputDecoration(
-                hintText: 'Digite seu relato',
-              ),
-              onChanged: (value) {
-                _relato = value;
-              },
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Container(
-              height: 60,
-              width: 230,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  stops: [0.2, 1],
-                  colors: [
-                    Colors.cyan[700]!,
-                    Color.fromRGBO(62, 71, 208, 1.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                          padding: EdgeInsets.only(left: 1, right: 2),
+                          decoration: new BoxDecoration(
+                            color: Colors.white,
+                          ),
+                          child: DropdownButton<String>(
+                            hint: Text(
+                              "                Categoria",
+                              style: GoogleFonts.breeSerif(
+                                textStyle:
+                                    Theme.of(context).textTheme.headline4,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w900,
+                                fontStyle: FontStyle.normal,
+                                color: Color.fromRGBO(62, 71, 208, 1.0),
+                              ),
+                            ),
+                            dropdownColor: Colors.white,
+                            icon: Icon(Icons.arrow_drop_down,
+                                color: Color.fromRGBO(62, 71, 208, 1.0)),
+                            iconSize: 30,
+                            style: GoogleFonts.breeSerif(
+                              textStyle: Theme.of(context).textTheme.headline4,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w900,
+                              fontStyle: FontStyle.normal,
+                              color: Color.fromRGBO(62, 71, 208, 1.0),
+                            ),
+                            value: valueChoose,
+                            onChanged: (newValue) {
+                              setState(() {
+                                valueChoose = newValue;
+                              });
+                            },
+                            items: listItem.map((valueItem) {
+                              return DropdownMenuItem(
+                                value: valueItem,
+                                child: Text(valueItem),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      width: 300,
+                      child: TextField(
+                        maxLength: 200,
+                        maxLines: 7,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Digite seu relato',
+                        ),
+                        onChanged: (value) {
+                          _relato = value;
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height: 45,
+                      width: 110,
+                      alignment: Alignment.centerRight,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          stops: [0.2, 1],
+                          colors: [
+                            Colors.cyan[700]!,
+                            Color.fromRGBO(62, 71, 208, 1.0),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        ),
+                      ),
+                      child: SizedBox.expand(
+                        child: FlatButton(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                "   Postar",
+                                style: GoogleFonts.breeSerif(
+                                  textStyle:
+                                      Theme.of(context).textTheme.headline4,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w900,
+                                  fontStyle: FontStyle.normal,
+                                  color: Colors.white,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
+                          onPressed: () {
+                            //Navigator.push(
+                            //context,
+                            //MaterialPageRoute(builder: (context) => PagePessoa()),
+                            //);
+                          },
+                        ),
+                      ),
+                    ),
                   ],
                 ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(30),
-                ),
               ),
-              child: SizedBox.expand(
-                child: FlatButton(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        "                Postar",
-                        style: GoogleFonts.breeSerif(
-                          textStyle: Theme.of(context).textTheme.headline4,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w900,
-                          fontStyle: FontStyle.normal,
-                          color: Colors.white,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                  onPressed: () {
-                    //Navigator.push(
-                    //context,
-                    //MaterialPageRoute(builder: (context) => PagePessoa()),
-                    //);
-                  },
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
