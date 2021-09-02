d69d16d67adc8e14f129aebb5197566ad101d0f4
eloise-takami <71730705+eloise-takami@users.noreply.github.com>
2021-09-02 18:08:51 -0300

    images erro/firebase_vazio

--- helpforyou/lib/shared/themes/app_images.dart
+++ helpforyou/lib/shared/themes/app_images.dart
@@ -3,4 +3,6 @@ class Imagem {
   static const logo_h = 'assets/images/h_sembranco.png';
   static const medico = 'assets/images/medico.png';
   static const borda = 'assets/images/borda2.png';
+  static const erro = 'assets/images/erro.png';
+  static const firebase_vazio = 'assets/images/firebase_vazio.png';
 }
